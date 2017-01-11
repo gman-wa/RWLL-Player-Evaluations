cfd48284278e8927d4934b080eea003312f62a70
gman-wa <garth.gillespie@gmail.com>
2017-01-10 21:34:09 -0800

    updates for 2017 v2 - css included with page, only Android for Roboto - 100/100 on speed test

--- inc/global-meta.php
+++ inc/global-meta.php
@@ -8,6 +8,8 @@
 ?>
 <meta charset="UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, minimal-ui" >
+<?php /*
 <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
-<link type="text/css" rel="stylesheet" href="http://cdn.cybernode.com/i/rwll/style-2017011001-min-gz.css" media="screen" />
+<link type="text/css" rel="stylesheet" href="http://cdn.cybernode.com/i/rwll/style-2017011001-min-gz.css" media="screen" /> */ ?>
+<style type="text/css"><?php include(getenv('DOCUMENT_ROOT') . "/assets/css/style-2017011001-min.css"); ?></style>
 <?php include(getenv('DOCUMENT_ROOT') . "/inc/apple-icons.php"); ?>
