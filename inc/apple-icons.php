af5ac6b10425e23b946621dcb9afa3953ed6f671
gman-wa <garth.gillespie@gmail.com>
2017-01-10 19:21:01 -0800

    updates for 2017 - sportsengine field, new mobile icons and css updates

--- inc/apple-icons.php
+++ inc/apple-icons.php
@@ -26,8 +26,10 @@ don't use landscape
 <meta name="apple-mobile-web-app-title" content="RWLL Evals" />
 <meta name="apple-mobile-web-app-capable" content="yes" />
 <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+<link rel="shortcut icon" href="http://cdn.cybernode.com/i/rwll/favicon.ico" type="image/x-icon" />
 <link rel="apple-touch-icon" href="http://cdn.cybernode.com/i/rwll/apple-touch-icon.png"/>
 <link rel="apple-touch-icon" sizes="57x57" href="http://cdn.cybernode.com/i/rwll/apple-touch-icon-57x57.png"/>
+<link rel="apple-touch-icon" sizes="57x57" href="http://cdn.cybernode.com/i/rwll/apple-touch-icon-60x60.png"/>
 <link rel="apple-touch-icon" sizes="72x72" href="http://cdn.cybernode.com/i/rwll/apple-touch-icon-72x72.png"/>
 <link rel="apple-touch-icon" sizes="76x76" href="http://cdn.cybernode.com/i/rwll/apple-touch-icon-76x76.png"/>
 <link rel="apple-touch-icon" sizes="114x114" href="http://cdn.cybernode.com/i/rwll/apple-touch-icon-114x114.png"/>
@@ -35,3 +37,11 @@ don't use landscape
 <link rel="apple-touch-icon" sizes="144x144" href="http://cdn.cybernode.com/i/rwll/apple-touch-icon-144x144.png"/>
 <link rel="apple-touch-icon" sizes="152x152" href="http://cdn.cybernode.com/i/rwll/apple-touch-icon-152x152.png"/>
 <link rel="apple-touch-icon" sizes="180x180" href="http://cdn.cybernode.com/i/rwll/apple-touch-icon-180x180.png"/>
+<link rel="icon" type="image/png" href="http://cdn.cybernode.com/i/rwll/favicon-32x32.png" sizes="32x32"/>
+<link rel="icon" type="image/png" href="http://cdn.cybernode.com/i/rwll/favicon-16x16.png" sizes="16x16"/>
+<link rel="icon" type="image/png" href="http://cdn.cybernode.com/i/rwll/favicon-96x96.png" sizes="96x96"/>
+<link rel="icon" type="image/png" href="http://cdn.cybernode.com/i/rwll/android-chrome-192x192.png" sizes="192x192"/>
+<meta name="msapplication-square70x70logo" content="http://cdn.cybernode.com/i/rwll/smalltile.png" />
+<meta name="msapplication-square150x150logo" content="http://cdn.cybernode.com/i/rwll/mediumtile.png" />
+<meta name="msapplication-wide310x150logo" content="http://cdn.cybernode.com/i/rwll/widetile.png" />
+<meta name="msapplication-square310x310logo" content="http://cdn.cybernode.com/i/rwll/largetile.png" />
