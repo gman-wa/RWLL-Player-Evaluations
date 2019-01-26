61c1ba9b3747a9c4249dcbcfdc920141dff55ab5
Garth Gillespie <garth.gillespie@thomsonreuters.com>
2019-01-25 21:52:26 -0800

    fixes for decimal input - float filters vs int

--- services/process_data.php
+++ services/process_data.php
@@ -20,11 +20,11 @@
     $request['fielding_iq'] = isset($_GET['fielding_iq']) && $_GET['fielding_iq'] != "" ? (int) filter_var($_GET['fielding_iq'], FILTER_SANITIZE_NUMBER_INT) : FALSE;
 
     $request['hitting_contact'] = isset($_GET['hitting_contact']) && $_GET['hitting_contact'] != "" ? (int) filter_var($_GET['hitting_contact'], FILTER_SANITIZE_NUMBER_INT) : FALSE;
-    $request['hitting_power'] = isset($_GET['hitting_power']) && $_GET['hitting_power'] != "" ? (int) filter_var($_GET['hitting_power'], FILTER_SANITIZE_NUMBER_INT) : FALSE;
+    $request['hitting_power'] = isset($_GET['hitting_power']) && $_GET['hitting_power'] != "" ? (float) filter_var($_GET['hitting_power'], FILTER_SANITIZE_NUMBER_FLOAT,FILTER_FLAG_ALLOW_FRACTION) : FALSE;
     $request['hitting_mechanics'] = isset($_GET['hitting_mechanics']) && $_GET['hitting_mechanics'] != "" ? (int) filter_var($_GET['hitting_mechanics'], FILTER_SANITIZE_NUMBER_INT) : FALSE;
     $request['hitting_baserunning'] = isset($_GET['hitting_baserunning']) && $_GET['hitting_baserunning'] != "" ? (float) filter_var($_GET['hitting_baserunning'], FILTER_SANITIZE_NUMBER_FLOAT,FILTER_FLAG_ALLOW_FRACTION) : FALSE;
 
-    $request['pitching_velocity'] = isset($_GET['pitching_velocity']) && $_GET['pitching_velocity'] != "" ? (int) filter_var($_GET['pitching_velocity'], FILTER_SANITIZE_NUMBER_INT) : FALSE;
+    $request['pitching_velocity'] = isset($_GET['pitching_velocity']) && $_GET['pitching_velocity'] != "" ? (float) filter_var($_GET['pitching_velocity'], FILTER_SANITIZE_NUMBER_FLOAT,FILTER_FLAG_ALLOW_FRACTION) : FALSE;
     $request['pitching_accuracy'] = isset($_GET['pitching_accuracy']) && $_GET['pitching_accuracy'] != "" ? (int) filter_var($_GET['pitching_accuracy'], FILTER_SANITIZE_NUMBER_INT) : FALSE;
     $request['pitching_mechanics'] = isset($_GET['pitching_mechanics']) && $_GET['pitching_mechanics'] != "" ? (int) filter_var($_GET['pitching_mechanics'], FILTER_SANITIZE_NUMBER_INT) : FALSE;
     $request['catching_disposed'] = isset($_GET['catching_disposed']) && $_GET['catching_disposed'] != "" ? (int) filter_var($_GET['catching_disposed'], FILTER_SANITIZE_NUMBER_INT) : FALSE;
